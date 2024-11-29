45cd0d31fdd3d3a0e211ba819317a48bc9a833e8
Dani Kim <202115057@sangmyung.kr>
2024-11-29 17:58:51 +0900

    Rearranged page structure

--- sensor/urls.py
+++ sensor/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', views.our_team, name='our_team'),
     path('getSensor/<int:cnt>', views.getSensor, name='getsensor'),
     path('setSensor', views.setSensor, name='setsensor'),
 
-    path('cane-description/', views.cane_description, name='cane_description'),
-    path('our-team/', views.our_team, name='our_team'),
+    # path('cane-description/', views.cane_description, name='cane_description'),
+    # path('our-team/', views.our_team, name='our_team'),
+    path('index/', views.index, name='index'),
 ]
